a2f0357b15f06b5afc00435b532cd28ac3765a66
Diana-Cano <dianacanoo@icloud.com>
2023-03-27 13:29:01 -0700

    completed activity

---
+++ paradigms.py
@@ -0,0 +1,65 @@
+#functional prompt 
+def flat_and_list(list):
+    flat_list = []
+    for int in list:
+        flatlist.append(element)
+print(list)
+
+def ascending_order(integers):
+    number = str(integers)
+    number = ''.join(sorted(number))
+    number = int(number)
+    return number
+
+integers = 187431047961
+print(ascending_order(integers))
+
+'''How does this solution ensure data immutability?
+Is this solution a pure function? Why or why not?
+yes , this would be considered a pure function because the return cakue would always be the same 
+if the same arguments were put in. Also, the function has no side effects making it a oure function.
+
+
+Is this solution a higher order function? Why or why not?
+Would it have been easier to solve this problem using a different programming style?
+
+Why in particular is functional programming a helpful paradigm when solving this problem?'''
+
+    
+#object oriented prompt
+
+class Podracer:
+    def __init__(self,max_speed,condition):
+        self.max_speed = max_speed
+        self.condition = condition 
+
+    def repair(self):
+        self.condition = "repaired"
+
+class AnakinsPod(Podracer):
+    def __init__(self,max_speed,condition)
+        super().__init__(max_speed,condition)
+
+    def boost(self):
+        self.max_speed *= 2 
+        
+
+class SebulbasPod(AnakinsPod):
+    def __init__(self, max_speed, condition)
+        super().__init__(max_speed, condition)
+    
+    def flame_jet():
+        self.condition = "trashed"
+
+'''How does this solution demonstrate the four pillars of OOP? (It may not demonstrate all of them, describe only those that apply)
+Encapsulation
+Abstraction
+Inheritance
+Polymorphism
+Would it have been easier to implement a solution to this problem using a different coding style? Why or why not?
+How in particular did Object Oriented Programming assist in the solving of this problem?'''
+    
+'''Is one of these coding paradigms "better" than the other? Why or why not?
+Given the opportunity to work predominantly using either of these coding paradigms, which seems more appealing? Why?
+Now being more familiar with these coding paradigms, what tasks/features/pieces of logic would be best handled using functional programming? Object Oriented Programming?
+Personally, which of these styles takes more work to understand? What should be done in order to deepen understanding related to this paradigm?'''
